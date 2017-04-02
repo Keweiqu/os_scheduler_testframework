b30276967b63a400419f997b55ca01429a26955d
Kewei Qu <kqu@brynmawr.edu>
2017-04-02 18:39:42 -0400

    parametrize dir

--- script/foo.py
+++ script/foo.py
@@ -2,13 +2,14 @@ import os, subprocess
 
 class Test:
     dir = "."
+    exec = "test"
     def setup(self, dir):
         self.dir = dir
         os.environ['LD_LIBRARY_PATH'] = dir
     def run(self):
-        p1 = subprocess.Popen(["gcc", "-o", "test", "../tests/0.c", "-L/home/kqu/cs355/proj4/kchang", "-luserthread"], env=os.environ, stdout=subprocess.PIPE)
+        p1 = subprocess.Popen(["gcc", "-o", self.exec, "../tests/0.c", "-L"+self.dir, "-luserthread"], env=os.environ, stdout=subprocess.PIPE)
         p1.wait()
-        p2 = subprocess.Popen(["./test"], env=os.environ, stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(["./"+self.exec], env=os.environ, stdout=subprocess.PIPE)
         p2.wait()
         stdout,_ = p2.communicate()
         print(stdout)
